af95012139e1d54bd21efbff1c08dc91a1620b0e
manavdevweb <peerthymanav@gmail.com>
2026-04-04 03:21:00 +0500

    Remove eslint from next.config.ts and ignore lint in build

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    eslint: {
-    ignoreDuringBuilds: true, // <- permet au build de passer même s'il y a des erreurs ESLint
-  },
 };
 
 export default nextConfig;
